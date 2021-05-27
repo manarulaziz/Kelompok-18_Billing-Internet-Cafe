76bd6fc013df38cf0c950e64f6a0894625143b5c
vincentiaputri72 <80144808+vincentiaputri72@users.noreply.github.com>
2021-05-27 20:38:58 +0700

    Update billingicafe_kelompok20.py

--- billingicafe_kelompok20.py
+++ billingicafe_kelompok20.py
@@ -1,1 +1,12 @@
-# start
+def menu_utama():
+    print("---------- MENU ----------")
+    print("[1] Membership")
+    print("[2] Transaksi")
+    print("[3] Pengaturan Akun")
+    print("[4] Keluar")
+
+    menu = input(">> ")
+    print("\n")
+
+if __name__ == '__main__':
+    menu_utama()
